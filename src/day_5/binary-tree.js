35632b3f24863afee34d3a9ba862cafd149a5f3c
manuasir <manu.asi2@gmail.com>
2021-04-06 01:40:18 +0200

    WIP: Get row

--- src/day_5/binary-tree.js
+++ src/day_5/binary-tree.js
@@ -1,56 +1,32 @@
 
-class Validator {
-  constructor(data) {
-    this.rows = data
-    this.required = {
-      'ecl': (value) => { return value === 'amb' || value === 'blu' || value === 'brn' || value === 'gry' || value === 'grn' || value === 'hzl' || value === 'oth' },
-      'pid': (value) => { return /^\d{9}$/.test(value) },
-      'eyr': (value) => { return Number(value) >= 2020 && Number(value) <= 2030 },
-      'hcl': (value) => { return /^#[0-9a-f]{6}$/.test(value) },
-      'byr': (value) => { return Number(value) >= 1920 && Number(value) <= 2002 },
-      'iyr': (value) => { return Number(value) >= 2010 && Number(value) <= 2020 },
-      'hgt': (value) => { return value.includes('cm') ? Number(value.split('cm')[0]) >= 150 && Number(value.split('cm')[0]) <= 193 : value.includes('in') ? Number(value.split('in')[0]) >= 59 && Number(value.split('in')[0]) <= 76 : false }
+
+class BinaryTree {
+  constructor() { }
+
+  getRow(label, range, index) {
+    const position = label[0]
+    if (index === 6) {
+      return position === 'F' ? Number(range.split('-')[0]) : Number(range.split('-')[1])
     }
-    this.optional = [
-      'cid'
-    ]
-  }
-
-  getRequired() {
-    return this.required
-  }
-
-  getFields(row) {
-    try {
-      return row.split(' ').map(item => item.split(':')[0])
-    } catch (error) {
-      console.error(error.message || error)
+    const left = label.slice(1, label.length)
+    let firstRange = ""
+    let secondRange = ""
+    if (position === 'F') {
+      firstRange = range.split('-')[0]
+      secondRange = Math.floor((Number(range.split('-')[1]) + Number(firstRange)) / 2)
+      const nextRange = `${firstRange}-${secondRange}`
+      index += 1
+      return this.getRow(left, nextRange, index)
+    } else if (position === 'B') {
+      firstRange = Math.ceil((Number(range.split('-')[1]) + Number(range.split('-')[0])) / 2)
+      secondRange = range.split('-')[1]
+      const nextRange = `${firstRange}-${secondRange}`
+      index += 1
+      return this.getRow(left, nextRange, index)
     }
   }
 
-  checkFields(fields) {
-    const diff = Object.keys(this.required).filter(value => !fields.includes(value))
-    return Array.isArray(diff) && (diff.length === 0 || (diff.length === 1 && this.optional.includes(diff[0])))
-  }
-
-  checkValues(row) {
-    const values = row.split(' ')
-    return values.map((item) => {
-      const key = item.split(':')[0]
-      const value = item.split(':')[1]
-      const res = key !== 'cid' ?
-        this.required[key](value) :
-        true
-      return res
-    }).reduce((a, b) => { return a && b })
-  }
-
-  countValidFields() {
-    return this.rows.map((row) => {
-      return this.checkFields(this.getFields(row)) && this.checkValues(row)
-    }).filter(item => item === true).length
-  }
 
 }
 
-module.exports = Validator
+module.exports = BinaryTree
--- test/day_5/tests_1.spec.js
+++ test/day_5/tests_1.spec.js
@@ -7,8 +7,31 @@ describe('Day 5', () => {
   const path = './test/day_5/mock.txt'
   const rows = loader.load(path)
   describe('Loader', () => {
-    test('Number of rows should be 3', () => {
-      expect(rows.length).toEqual(3)
+    test('Number of rows should be 4', () => {
+      expect(rows.length).toEqual(4)
     })
+  })
+  describe('getRow first mock', () => {
+    test('Position should be 44', () => {
+      const tree = new BinTree()
+      const result = tree.getRow(rows[0],'0-127',0)
+      expect(result).toEqual(44)
+    })
+    test('Position should be 70', () => {
+      const tree = new BinTree()
+      const result = tree.getRow(rows[1],'0-127',0)
+      expect(result).toEqual(70)
+    })
+    test('Position should be 14', () => {
+      const tree = new BinTree()
+      const result = tree.getRow(rows[2],'0-127',0)
+      expect(result).toEqual(14)
+    })
+    test('Position should be 102', () => {
+      const tree = new BinTree()
+      const result = tree.getRow(rows[3],'0-127',0)
+      expect(result).toEqual(102)
+    })
+
   })
 })

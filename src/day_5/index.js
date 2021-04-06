108d9c52884588b49fe9a3680b8747d69dfe257b
manuasir <manu.asi2@gmail.com>
2021-04-06 02:17:00 +0200

    Day 5

--- src/day_5/index.js
+++ src/day_5/index.js
@@ -9,5 +9,7 @@ const ids = seats.map((seat) => {
 })
 
 const highestId = Math.max(...ids)
-
 console.log('Highest ID: ',highestId)
+const [min,max] = [Math.min(...ids.sort()), Math.max(...ids.sort())];
+const out = Array.from(Array(max-min),(v,i)=>i+min).filter(i=>!ids.sort().includes(i));
+console.log('Actual seat: ',out)

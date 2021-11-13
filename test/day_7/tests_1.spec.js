8e783900c51ccc513bf0526a32a7f2260f24440c
manuasir <manu.asi2@gmail.com>
2021-11-13 03:58:16 +0100

    test(day 7): add test

--- test/day_7/tests_1.spec.js
+++ test/day_7/tests_1.spec.js
@@ -2,12 +2,13 @@
 
 const Loader = require('../../src/day_7/loader.js')
 const Luggage = require('../../src/day_7/luggage.js')
-
 describe('Day 7', () => {
   const loader = new Loader()
   const path = './test/day_7/mock.txt'
   const result = loader.load(path)
+  const data = loader.load('./src/day_7/data')
   const luggage = new Luggage(result)
+  const finalLuggage = new Luggage(data)
   describe('Loader', () => {
     it('Number of rows', () => {
       expect(result.length).toEqual(9)
@@ -18,5 +19,10 @@ describe('Day 7', () => {
       const result = luggage.getAllBags(`shiny gold`)
       expect(result).toEqual(4)
     })
+    it('Number of occurrences should be 254', () => {
+      const result = finalLuggage.getAllBags(`shiny gold`)
+      expect(result).toEqual(254)
+    })
   })
+
 })
